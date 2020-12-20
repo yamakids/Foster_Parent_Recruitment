2ce5084bb8bff7a28de963e8caf00b1729ddfc99
yamakids <e101.k202.d404@gmail.com>
2020-12-21 00:17:42 +0900

    Implement new function

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Wish;
+use App\models\Wish;
 use Illuminate\Pagination\Paginator;
 
 class PageController extends Controller
